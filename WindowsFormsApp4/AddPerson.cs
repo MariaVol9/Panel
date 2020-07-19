bc67e67674403622c06fb36e0e66ebfd4d7dcb51
margolevi977@gmail.com <margolevi977@gmail.com>
2020-07-19 16:28:43 +0300

    Add project files.

---
+++ WindowsFormsApp4/AddPerson.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using dbApp.Entities;
+using dbApp.API;
+
+namespace WindowsFormsApp4
+{
+    public partial class AddPerson : UserControl
+    {
+        public AddPerson()
+        {
+            InitializeComponent();
+        }
+
+        private void SavePerson_Click(object sender, EventArgs e)
+        {
+            Person person = new Person();
+            person.id = int.Parse(Id.Text);
+
+
+
+
+            CustomerAPI customerAPI = new CustomerAPI();
+            customerAPI.SaveCustomer(person);
+        }
+    }
+}
